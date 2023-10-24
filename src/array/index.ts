6e2a547540ac8e4864d6b6f781169ea5091032c4
Aiden-FE <aiden_jin@outlook.com>
2023-10-24 17:20:35 +0800

    feat: init rotate matrix

--- src/array/index.ts
+++ src/array/index.ts
@@ -1,3 +1,4 @@
 export { default as findTheMiddleIndexInArray } from './find-the-middle-index-in-array/find-the-middle-index-in-array';
 export { default as searchInsertIndex } from './search-insert-index/search-insert-index';
 export { default as mergeOverlapIntervals } from './merge-overlap-intervals/merge-overlap-intervals';
+export { default as rotateMatrix } from './rotate-matrix/rotate-matrix';
