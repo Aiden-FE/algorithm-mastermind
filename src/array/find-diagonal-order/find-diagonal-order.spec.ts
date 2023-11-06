1ca117695dbb1be95d02e4d289d4e7926242e78b
Aiden-FE <aiden_jin@outlook.com>
2023-11-06 14:51:01 +0800

    feat: used two pointers to solve find-diagonal-order

--- src/array/find-diagonal-order/find-diagonal-order.spec.ts
+++ src/array/find-diagonal-order/find-diagonal-order.spec.ts
@@ -2,21 +2,21 @@ import findDiagonalOrder from './find-diagonal-order';
 
 describe('对角线遍历', () => {
   it('should be "[1, 2, 4, 7, 5, 3, 6, 8, 9]"', () => {
-    expect(
-      findDiagonalOrder([
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-      ]),
-    ).toMatchObject([1, 2, 4, 7, 5, 3, 6, 8, 9]);
+    const data = findDiagonalOrder([
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ]);
+    console.log('debug: ', data);
+    expect(data).toMatchObject([1, 2, 4, 7, 5, 3, 6, 8, 9]);
   });
 
   it('should be "[1, 2, 3, 4]"', () => {
-    expect(
-      findDiagonalOrder([
-        [1, 2],
-        [3, 4],
-      ]),
-    ).toMatchObject([1, 2, 3, 4]);
+    const data = findDiagonalOrder([
+      [1, 2],
+      [3, 4],
+    ]);
+    console.log('debug2: ', data);
+    expect(data).toMatchObject([1, 2, 3, 4]);
   });
 });
--- src/array/find-diagonal-order/find-diagonal-order.ts
+++ src/array/find-diagonal-order/find-diagonal-order.ts
@@ -11,6 +11,41 @@
  * @param mat
  * @inheritDoc https://leetcode.cn/leetbook/read/array-and-string/cuxq3/
  */
+// 执行用时：88 ms 内存消耗：47.6 MB 双指针解法 时间复杂度O(m*n) 空间复杂度O(3)
 export default function findDiagonalOrder(mat: number[][]): number[] {
-  return [];
+  let row = 0;
+  let col = 0;
+  const result: number[] = [];
+  // 根据对角线长度遍历
+  while (row < mat.length && col < mat[0].length) {
+    result.push(mat[row][col]);
+    // 偶数向上移动指针
+    if ((row + col) % 2 === 0) {
+      // 当列标大于等于 n,说明右侧已经触边,下移行标
+      if (col >= mat[0].length - 1) {
+        row += 1;
+      } else if (row === 0) {
+        // 当列标等于 0,说明顶部已经触边,后移列标
+        col += 1;
+      } else {
+        // 沿对角线方向移动
+        row -= 1;
+        col += 1;
+      }
+    } else {
+      // 当行标大于等于 m,说明底部已经触边,右移列标
+      // eslint-disable-next-line no-lonely-if
+      if (row >= mat.length - 1) {
+        col += 1;
+      } else if (col === 0) {
+        // 当列标为0,说明左侧已经触边,下移行标
+        row += 1;
+      } else {
+        // 沿对角线方向移动
+        row += 1;
+        col -= 1;
+      }
+    }
+  }
+  return result;
 }
